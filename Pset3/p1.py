0096bdd77d27b1dc4757be54a05021e48de4f11e
youssefelmasry <Y.ELMasry@nu.edu.eg>
2018-10-21 00:03:55 +0200

    pset3
    problem1

---
+++ Pset3/p1.py
@@ -0,0 +1,8 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    return set(list(secretWord)).intersection(lettersGuessed) == set(secretWord)
