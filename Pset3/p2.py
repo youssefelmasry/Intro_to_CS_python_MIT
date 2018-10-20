ef7c51eb10f008a0d9a9e982ec5e3ea34b322cc7
youssefelmasry <Y.ELMasry@nu.edu.eg>
2018-10-21 00:05:57 +0200

    pset3
    problem2

---
+++ Pset3/p2.py
@@ -0,0 +1,14 @@
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    res = ''
+    for i in secretWord:
+        if i in lettersGuessed:
+            res += i
+        else:
+            res += '_'
+    return res    
